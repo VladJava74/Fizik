6a3073b80c43d734160532891ee721ff50e96309
VladJava74 <161621666+VladJava74@users.noreply.github.com>
2024-02-28 20:55:37 +0500

    Add files via upload

---
+++ Fizik/src/test/java/com/example/Fizik/FizikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fizik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
